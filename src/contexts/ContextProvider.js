916ed58fb720287f6e9135597be6f75b46d6ceda
Arujan <aruzhanbekmakhanbetkyzy@gmail.com>
2022-08-26 03:03:20 +0900

    contextProvider

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -8,12 +8,20 @@ const initialState = {
     cart: false,
     userProfile: false,
     notification: false,
-}
+};
 
-export  const ContextProvider = ({ children }) => {
+export const ContextProvider = ({ children }) => {
+    const [activeMenu, setActiveMenu] = useState(true);
+
     return (
-        <StateContext.Provider value={{ test: 'test' }}>
+        <StateContext.Provider
+            value={{
+                activeMenu,
+            }}
+        >
             {children}
         </StateContext.Provider>
-    )
-}
+    );
+};
+
+export const useStateContext = () => useContext(StateContext);
